7edbf6feb41d365a0a274963eafbf3985cc8224e
wjbittle <wjbittle@e3994c14-1342-11df-8fcd-d121000d4eca>
2014-01-06 11:33:40 +0000

    Checking in the test case for issue 10;

--- trunk/junit/org/dyn4j/geometry/SweepLineTest.java
+++ trunk/junit/org/dyn4j/geometry/SweepLineTest.java
@@ -408,4 +408,19 @@ public class SweepLineTest extends AbstractDecomposeTest {
 		// the result should have n - 2 triangles shapes
 		TestCase.assertEquals(vertices.length - 2, result.size());
 	}
+	
+	/**
+	 * Tests the triangulation implementation against the zoom1 data file.
+	 * @since 3.1.9
+	 */
+	@Test
+	public void triangulateSuccessZoom2() {
+		Vector2[] vertices = this.load(SweepLineTest.class.getResourceAsStream("/org/dyn4j/data/zoom2.dat"));
+		
+		// decompose the poly
+		List<? extends Convex> result = this.algo.triangulate(vertices);
+		
+		// the result should have n - 2 triangles shapes
+		TestCase.assertEquals(vertices.length - 2, result.size());
+	}
 }
