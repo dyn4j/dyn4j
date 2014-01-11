8c6a2769c696509591f778da224673bd89476a27
wjbittle <wjbittle@e3994c14-1342-11df-8fcd-d121000d4eca>
2014-01-11 21:27:25 +0000

    Fixed a bug in the SweepLine class where it would enter an infinite loop due to a poor 'closest edge to vertex' pre condition that was there to improve performance (it turns out it wasn't a sufficient check);  Added the same test cases to other algorithms for completeness;

--- trunk/junit/org/dyn4j/geometry/EarClippingTest.java
+++ trunk/junit/org/dyn4j/geometry/EarClippingTest.java
@@ -430,4 +430,34 @@ public class EarClippingTest extends AbstractDecomposeTest {
 		// the result should have n - 2 triangles shapes
 		TestCase.assertEquals(vertices.length - 2, result.size());
 	}
+	
+	/**
+	 * Tests the implementation against the zoom3 data file.
+	 * @since 3.1.9
+	 */
+	@Test
+	public void successZoom3() {
+		Vector2[] vertices = this.load(EarClippingTest.class.getResourceAsStream("/org/dyn4j/data/zoom3.dat"));
+		
+		// decompose the poly
+		List<? extends Convex> result = this.algo.decompose(vertices);
+		
+		// the result should have n - 2 triangles shapes
+		TestCase.assertTrue(result.size() <= vertices.length - 2);
+	}
+	
+	/**
+	 * Tests the triangulation implementation against the zoom3 data file.
+	 * @since 3.1.9
+	 */
+	@Test
+	public void triangulateSuccessZoom3() {
+		Vector2[] vertices = this.load(EarClippingTest.class.getResourceAsStream("/org/dyn4j/data/zoom3.dat"));
+		
+		// decompose the poly
+		List<? extends Convex> result = this.algo.triangulate(vertices);
+		
+		// the result should have n - 2 triangles shapes
+		TestCase.assertEquals(vertices.length - 2, result.size());
+	}
 }
