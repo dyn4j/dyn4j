b2ccdbd1835e24f35672bcdf693425903afda986
William Bittle <wjbittle@gmail.com>
2016-04-29 23:14:27 -0400

    Internal edge fixes

--- src/org/dyn4j/collision/narrowphase/LinkPostProcessor.java
+++ src/org/dyn4j/collision/narrowphase/LinkPostProcessor.java
@@ -23,90 +23,257 @@ public class LinkPostProcessor {
 		
 		Vector2 p1 = tx1.getTransformed(link.getPoint1());
 		Vector2 p2 = tx1.getTransformed(link.getPoint2());
-		Vector2 p1p2 = p1.to(p2);
+		Vector2 p0 = link.getPoint0() != null ? tx1.getTransformed(link.getPoint0()) : null;
+		Vector2 p3 = link.getPoint3() != null ? tx1.getTransformed(link.getPoint3()) : null;
 		
-		double ndp1p2 = n.dot(p1p2);
+		boolean convex1 = false;
+		boolean convex2 = false;
 		
-		if (link.getPoint0() != null && ndp1p2 < 0) {
-			Vector2 p0 = tx1.getTransformed(link.getPoint0());
-			Vector2 p0p1 = p0.to(p1);
-			double t1 = n.dot(p1p2);
-			double t2 = n.dot(p0p1);
-			// is the normal in ok zone?
-			if (!(t1 < 0 && t2 > 0)) {
-				// nope, so we need to adjust it
-				if (p0p1.cross(p1p2) < 0) {
-					// convex
-					System.out.println("case2");
-					Vector2 newN = getCorrectedNormal(p0p1, p0, c);
-					adjust(p, newN);
-//					System.out.println(p.getNormal());
-//					System.out.println(p.getDepth());
-					return;
-				} else if (p0p1.cross(p1p2) >= 0) {
-					// concave
-					System.out.println("case4");
-					Vector2 newN = getCorrectedNormal(p1p2, p1, c);
-					adjust(p, newN);
-//					System.out.println(p.getNormal());
-//					System.out.println(p.getDepth());
-					return;
-				}
-			}
+		Vector2 normal = null;
+		Vector2 edge0 = null;
+		Vector2 edge1 = null;
+		Vector2 edge2 = null;
+		Vector2 normal0 = null;
+		Vector2 normal1 = null;
+		Vector2 normal2 = null;
+		Vector2 upper = null;
+		Vector2 lower = null;
+		
+		double offset0 = 0;
+		double offset1 = 0;
+		double offset2 = 0;
+		
+		edge1 = p1.to(p2);
+		edge1.normalize();
+		normal1 = edge1.getLeftHandOrthogonalVector();
+		offset1 = normal1.dot(p1.to(c));
+		
+		if (p0 != null) {
+			edge0 = p0.to(p1);
+			edge0.normalize();
+			// get the normal for p0->p1
+			normal0 = edge0.getLeftHandOrthogonalVector();
+			// does p0->p1->p2 make a convex feature?
+			convex1 = edge0.cross(edge1) >= 0;
+			// where is the center of the other shape
+			offset0 = normal0.dot(p0.to(c));
 		}
 		
-		if (link.getPoint3() != null && ndp1p2 > 0) {
-			Vector2 p3 = tx1.getTransformed(link.getPoint3());
-			Vector2 p2p3 = p2.to(p3);
-			double t1 = n.dot(p1p2);
-			double t2 = n.dot(p2p3);
-			// is the normal in ok zone?
-			if (!(t1 > 0 && t2 < 0)) {
-				// nope, so we need to adjust it
-				if (p1p2.cross(p2p3) < 0) {
-					// convex
-					System.out.println("case1");
-					Vector2 newN = getCorrectedNormal(p2p3, p2, c);
-					adjust(p, newN);
-//					System.out.println(p.getNormal());
-//					System.out.println(p.getDepth());
-					return;
-				} else if (p1p2.cross(p2p3) >= 0) {
-					// concave
-					System.out.println("case3");
-					Vector2 newN = getCorrectedNormal(p1p2, p1, c);
-					adjust(p, newN);
-//					System.out.println(p.getNormal());
-//					System.out.println(p.getDepth());
-					return;
+		if (p3 != null) {
+			edge2 = p2.to(p3);
+			edge2.normalize();
+			// get the normal for p2->p3
+			normal2 = edge2.getLeftHandOrthogonalVector();
+			// does p1->p2->p3 make a convex feature?
+			convex2 = edge1.cross(edge2) >= 0;
+			// where is the center of the other shape
+			offset2 = normal2.dot(p2.to(c));
+		}
+		
+		// do we have both previous and next vertices?
+		if (p0 != null && p3 != null) {
+			// are both features convex?
+			if (convex1 && convex2) {
+				//   O----O
+				//  /      \
+				// O        O
+				// is the center of the other shape on the convex side or the concave side?
+				boolean front = offset0 >= 0 || offset1 >= 0 || offset2 >= 0;
+				if (front) {
+					// its on the outside (convex), so the normal has be between the
+					// normal of p0->p1 and p2->p3
+					normal = normal1;
+					lower = normal0;
+					upper = normal2;
+				} else {
+					// its on the inside (concave), so just use the normal of the segment
+					normal = normal1.getNegative();
+					lower = normal;
+					upper = normal;
+				}
+			} else if (convex1) {
+				//          O
+				//         /
+				//   O----O
+				//  /
+				// O 
+				// only p0->p1->p2 is convex
+				// is the center of the other shape on the convex side or the concave side?
+				boolean front = offset0 >= 0 || (offset1 >= 0 && offset2 >= 0);
+				if (front) {
+					// its on the convex side, the normal must be between the normals of
+					// p0->p1 and p1->p2
+					normal = normal1;
+					lower = normal0;
+					upper = normal1;
+				} else {
+					// its on the concave side, the normal must be between the normals of
+					// p1->p2 and p2->p3 on the convex side of p1->p2->p3
+					normal = normal1.getNegative();
+					lower = normal2.getNegative();
+					upper = normal1.getNegative();
+				}
+			} else if (convex2) {
+				// O
+				//  \
+				//   O----O
+				//         \
+				//          O
+				// only p1->p2->p3 is convex
+				// is the center of the other shape on the convex side or the concave side?
+				boolean front = offset2 >= 0 || (offset0 >= 0 && offset1 >= 0);
+				if (front) {
+					// its on the convex side, the normal must be in between the normals of
+					// p1->p2 and p2->p3
+					normal = normal1;
+					lower = normal1;
+					upper = normal2;
+				} else {
+					// its on the concave side, the normal must be between the normals of
+					// p0->p1 and p1->p2 on the convex side of p0->p1->p2
+					normal = normal1.getNegative();
+					lower = normal1.getNegative();
+					upper = normal0.getNegative();
+				}
+			} else {
+				// O        O
+				//  \      /
+				//   O----O
+				// otherwise both are convex
+				// is the center of the other shape on the convex or concave side?
+				boolean front = offset0 >= 0 && offset1 >= 0 && offset2 >= 0;
+				if (front) {
+					// its on the concave side, so just use the normal of the segment
+					normal = normal1;
+					lower = normal1;
+					upper = normal1;
+				} else {
+					// its on the convex side, so the normal must be between the normals of
+					// p0->p1 and p2->p3
+					normal = normal1.getNegative();
+					lower = normal2.getNegative();
+					upper = normal0.getNegative();
 				}
 			}
-		}
-	}
-	
-	private static final Vector2 getCorrectedNormal(Vector2 edge, Vector2 edgePoint1, Vector2 center) {
-		Vector2 sp = edgePoint1.to(center);
-		// determine what side of the edge the other shape's center
-		// is on so we know what normal of the edge to use
-		if (edge.cross(sp) >= 0) {
-			return edge.right();
+		} else if (p0 != null) {
+			// we only have a previous point
+			// does p0->p1->p2 make a convex feature?
+			if (convex1) {
+				//   O----O
+				//  /
+				// O
+				// is the center of the other shape on the convex or concave side?
+				boolean front = offset0 >= 0 || offset1 >= 0;
+				if (front) {
+					// its on the convex side, so the normal must be between the normals of
+					// p0->p1 and p1->p2 rotated -pi
+					normal = normal1;
+					lower = normal0;
+					upper = normal1.getNegative();
+				} else {
+					// its on the concave side, so the normal must be between the normals of
+					// p1->p2 and p1->p2 rotated -pi
+					normal = normal1.getNegative();
+					lower = normal1;
+					upper = normal1.getNegative();
+				}
+			} else {
+				// O
+				//  \
+				//   O----O
+				// is the center of the other shape on the convex or concave side?
+				boolean front = offset0 >= 0 && offset1 >= 0;
+				if (front) {
+					// its on the concave side, so the normal must be between the normals of
+					// p1->p2 and p1->p2 rotated -pi
+					normal = normal1;
+					lower = normal1;
+					upper = normal1.getNegative();
+				} else {
+					// its on the convex side, so the normal must be between the normals of
+					// p1->p2 and p0->p1 rotated -pi
+					normal = normal1.getNegative();
+					lower = normal1;
+					upper = normal0.getNegative();
+				}
+			}
+		} else if (p3 != null) {
+			// we only have a next point
+			// does p1->p2->p3 make a convex feature?
+			if (convex2) {
+				// O----O
+				//       \
+				//        O
+				// is the center of the other shape on the convex or concave side?
+				boolean front = offset1 >= 0 || offset2 >= 0;
+				if (front) {
+					// its on the convex side, so the normal must be between the normals of
+					// p1->p2 rotated -pi and p2->p3
+					normal = normal1;
+					lower = normal1.getNegative();
+					upper = normal2;
+				} else {
+					// its on the concave side, so the normal must be between the normals of
+					// p1->p2 rotated -pi and p1->p2
+					normal = normal1.getNegative();
+					lower = normal1.getNegative();
+					upper = normal1;
+				}
+			} else {
+				//        O
+				//       /
+				// O----O
+				// is the center of the other shape on the convex or concave side?
+				boolean front = offset1 >= 0 && offset2 >= 0;
+				if (front) {
+					// its on the concave side, so the normal must be between the normals of
+					// p1->p2 rotated -pi and p1->p2
+					normal = normal1;
+					lower = normal1.getNegative();
+					upper = normal1;
+				} else {
+					// its on the convex side, so the normal must be between the normals of
+					// p2->p3 rotated -pi and p1->p2
+					normal = normal1.getNegative();
+					lower = normal2.getNegative();
+					upper = normal1;
+				}
+			}
 		} else {
-			return edge.left();
+			// otherwise we don't have any adjacency information
+			boolean front = offset1 >= 0;
+			if (front) {
+				normal = normal1;
+				lower = normal1.getNegative();
+				upper = normal1.getNegative();
+			} else {
+				normal = normal1.getNegative();
+				lower = normal1;
+				upper = normal1;
+			}
 		}
-	}
-	
-	private static final void adjust(Penetration p, Vector2 newNormal) {
-		newNormal.normalize();
-		// project the old normal onto the new normal to obtain
-		// an estimated depth and take the absolute value
-		// so that if the normals are opposite we still get a positive depth
-		double newD = Math.abs(p.getNormal().dot(newNormal) * p.getDepth());
-		if (newD == 0.0) {
-			// the depth will be zero if the old normal and the new normal
-			// are perpendicular, so just use the original depth
-			newD = p.getDepth();
+		
+		//System.out.println("n: " + n + " nn: " + normal + " l: " + lower + " u: " + upper);
+		
+		// determine which normal, the upper or the lower, we should rotate the
+		// collision normal towards
+		Vector2 perp = normal.getRightHandOrthogonalVector();
+		if (n.dot(perp) >= 0) {
+			// use the upper normal
+			p.normal = upper;
+			// adjust the depth
+			p.depth = upper.dot(n) * p.depth;
+		} else {
+			// use the lower normal
+			p.normal = lower;
+			// adjust the depth
+			p.depth = lower.dot(n) * p.depth;
 		}
-		p.setNormal(newNormal);
-		p.setDepth(newD);
+		
+		// make sure the adjusted normal is pointing in
+		// the same direction as the collision normal
+		if (n.dot(p.normal) < 0) {
+			p.normal.negate();
+		}
 	}
 }
