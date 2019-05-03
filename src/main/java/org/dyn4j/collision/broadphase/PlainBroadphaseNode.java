89cbe9cdec4196dc1c679e432570b387cfdb1d14
mtsamis <tsamismanolis@gmail.com>
2019-05-03 21:29:34 +0300

    Initial commit for PlainBroadphase detector

---
+++ src/main/java/org/dyn4j/collision/broadphase/PlainBroadphaseNode.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2010-2016 William Bittle  http://www.dyn4j.org/
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted 
+ * provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice, this list of conditions 
+ *     and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+ *     and the following disclaimer in the documentation and/or other materials provided with the 
+ *     distribution.
+ *   * Neither the name of dyn4j nor the names of its contributors may be used to endorse or 
+ *     promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.dyn4j.collision.broadphase;
+
+import org.dyn4j.collision.Collidable;
+import org.dyn4j.collision.Fixture;
+import org.dyn4j.geometry.AABB;
+import org.dyn4j.geometry.Transform;
+
+/**
+ * Simple helper class that holds information for each item in the {@link PlainBroadphase}.
+ * 
+ * @author Manolis Tsamis
+ * @version 3.3.1
+ * @since 3.3.1
+ * @param <E> the {@link Collidable} type
+ * @param <T> the {@link Fixture} type
+ */
+class PlainBroadphaseNode<E extends Collidable<T>, T extends Fixture> {
+	public final E collidable;
+	public final T fixture;
+	public AABB aabb;
+	boolean tested;
+
+	PlainBroadphaseNode(E collidable, T fixture) {
+		this.collidable = collidable;
+		this.fixture = fixture;
+		
+		// calculate the initial AABB
+		updateAABB();
+	}
+	
+	/**
+	 * Updates the AABB of this node
+	 */
+	void updateAABB() {
+		// Remember, we don't expand the AABB
+		Transform tx = collidable.getTransform();
+		this.aabb = fixture.getShape().createAABB(tx);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("PlainBroadphaseNode[AABB=").append(this.aabb.toString())
+		.append("|Fixture=").append(this.fixture.getId())
+		.append("]");
+		return sb.toString();
+	}
+}
