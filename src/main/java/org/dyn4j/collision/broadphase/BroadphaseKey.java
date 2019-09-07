6e703b2306fe76aa2f3af76b2199bec4de099129
William Bittle <wjbittle@gmail.com>
2019-09-06 20:22:45 -0400

    Fix for small bug in the BroadphaseKey equals method

--- src/main/java/org/dyn4j/collision/broadphase/BroadphaseKey.java
+++ src/main/java/org/dyn4j/collision/broadphase/BroadphaseKey.java
@@ -74,7 +74,7 @@ final class BroadphaseKey {
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null) return false;
-		if (obj == this) return false;
+		if (obj == this) return true;
 		if (obj instanceof BroadphaseKey) {
 			BroadphaseKey key = (BroadphaseKey)obj;
 			return key.collidable.equals(this.collidable) &&
